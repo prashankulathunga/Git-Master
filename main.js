7fcc220d0d1363c97faded6fa29c41549b2af2e0
prashankulathunga <prashanrandikakulathunga@gmail.com>
2025-06-29 23:07:46 +0530

    code loop

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('Hello Coder!');
+console.log("Hello World");
+
+const array = [1,2,3,4,5,6];
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    console.log(element); 
+    
+}
